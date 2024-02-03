1a2f6a7095861e577bddcd3abadda0b10e86d034
Larry Lo <gaogaigargalden@gmail.com>
2024-02-03 15:12:04 +0800

    feat: update dart to the latest version

--- lib/game/game_over/config.dart
+++
@@ -1,6 +0,0 @@
-class GameOverConfig {
-  late final double textWidth = 382.0;
-  late final double textHeight = 25.0;
-  late final double restartWidth = 72.0;
-  late final double restartHeight = 64.0;
-}
--- lib/game/obstacle/config.dart
+++
@@ -1,4 +0,0 @@
-class ObstacleConfig {
-  late final int maxObstacleLength = 3;
-  late final double maxGapCoefficient = 1.5;
-}
